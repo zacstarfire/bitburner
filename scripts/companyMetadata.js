d71536299046699677f31d4bd3238c00041bab7c
Zac Starfire <115726722+zacstarfire@users.noreply.github.com>
2022-10-13 11:39:50 -0600

    Create companyMetadata.js

---
+++ scripts/companyMetadata.js
@@ -0,0 +1,342 @@
+//
+//  Company Lookup Metadata
+//
+
+export const companyMeta = [
+	{
+		'companyName': '.',
+		'stockSymbol': '',
+		'serverName': '.',
+	},
+	{
+		'companyName': 'AeroCorp',
+		'stockSymbol': 'AERO',
+		'serverName': 'aerocorp',
+	},
+	{
+		'companyName': 'Aevum Police Headquarters',
+		'stockSymbol': '',
+		'serverName': 'aevum-police',
+	},
+	{
+		'companyName': 'Alpha Enterprises',
+		'stockSymbol': 'APHE',
+		'serverName': 'alpha-ent'
+	},
+	{
+		'companyName': 'Applied Energetics',
+		'stockSymbol': 'APHE',
+		'serverName': 'applied-energetics'
+	},
+	{
+		'companyName': 'Bachman & Associates',
+		'stockSymbol': '', 
+		'serverName': 'b-and-a'
+	},
+	{
+		'companyName': 'Blade Industries',
+		'stockSymbol': 'BLD',
+		'serverName': 'blade'
+	},
+	{
+		'companyName': 'Catalyst Ventures',
+		'stockSymbol': 'CTYS',
+		'serverName': 'catalyst'
+	},
+	{
+		'companyName': 'Clarke Incorporated',
+		'stockSymbol': 'CLRK',
+		'serverName': 'clarkinc'
+	},
+	{
+		'companyName': 'Crush Fitness',
+		'stockSymbol': '',
+		'serverName': 'crush-fitness',
+	},
+	{
+		'companyName': 'CompuTek',
+		'stockSymbol': 'CTK',
+		'serverName': 'computek'
+	},
+	{
+		'companyName': 'DefComm',
+		'stockSymbol': 'DCOMM',
+		'serverName': 'defcomm'
+	},
+	{
+		'companyName': 'DeltaOne',
+		'stockSymbol': '',
+		'serverName': 'deltaone'
+	},
+	{
+		'companyName': 'ECorp',
+		'stockSymbol': 'ECP',
+		'serverName': 'ecorp'
+	},
+	{
+		'companyName': 'Four Sigma',
+		'stockSymbol': 'FSIG',
+		'serverName': '4sigma'
+	},
+	{
+		'companyName': 'Fulcrum Technologies',
+		'stockSymbol': 'FLCM',
+		'serverName': 'fulcrumassets'
+	},
+	{
+		'companyName': 'FoodNStuff',
+		'stockSymbol': 'FNS',
+		'serverName': 'foodnstuff'
+	},
+	{
+		'companyName': 'Galactic Cybersystems',
+		'stockSymbol': '',
+		'serverName': 'galactic-cyber'
+	},
+	{
+		'companyName': 'Global Pharmaceuticals',
+		'stockSymbol': 'GPH',
+		'serverName': 'global-pharm'
+	},
+	{
+		'companyName': 'HaraKiri Sushi Bar Network',
+		'stockSymbol': '',
+		'serverName': 'harakiri-sushi',
+	},
+	{
+		'companyName': 'HongFang Teahouse',
+		'stockSymbol': '',
+		'serverName': 'hong-fang-tea',
+	},
+	{
+		'companyName': 'Icarus Microsystems',
+		'stockSymbol': 'ICRS',
+		'serverName': 'icarus'
+	},
+	{
+		'companyName': 'InfoComm',
+		'stockSymbol': '',
+		'serverName': 'infocomm',
+	},
+	{
+		'companyName': 'Iron Gym Network',
+		'stockSymbol': '',
+		'serverName': 'iron-gym',
+	},
+	{
+		'companyName': 'Helios',
+		'stockSymbol': '',
+		'serverName': 'The-Cave'
+	},
+	{
+		'companyName': 'Helios Labs',
+		'stockSymbol': 'HLS',
+		'serverName': 'helios'
+	},
+	{
+		'companyName': "Joe's Guns",
+		'stockSymbol': 'JGN',
+		'serverName': 'joesguns',
+	},
+	{
+		'companyName': 'Johnson Orthopedics',
+		'stockSymbol': '',
+		'serverName': 'johnson-ortho',
+	},
+	{
+		'companyName': 'KuaiGong International',
+		'stockSymbol': 'KGI',
+		'serverName': 'kuai-gong'
+	},
+	{
+		'companyName': 'LexoCorp',
+		'stockSymbol': 'LXO',
+		'serverName': 'lexo-corp'
+	},
+	{
+		'companyName': "Max Hardware Store",
+		'stockSymbol': '',
+		'serverName': 'max-hardware',
+	},
+	{
+		'companyName': 'MegaCorp',
+		'stockSymbol': 'MGCP',
+		'serverName': 'megacorp'
+	},
+	{
+		'companyName': 'Microdyne Technologies',
+		'stockSymbol': 'MDYN',
+		'serverName': 'microdyne'
+	},
+	{
+		'companyName': 'Millenium Fitness Network',
+		'stockSymbol': '',
+		'serverName': 'millenium-fitness',
+	},
+	{
+		'companyName': 'Nectar Nightclub Network',
+		'stockSymbol': '',
+		'serverName': 'nectar-net',
+	},
+	{
+		'companyName': 'Neo Nightclub Network',
+		'stockSymbol': '',
+		'serverName': 'neo-net'
+	},
+	{
+		'companyName': 'NetLink Technologies',
+		'stockSymbol': 'NTLK',
+		'serverName': 'netlink'
+	},
+	{
+		'companyName': 'Noodle Bar',
+		'stockSymbol': '',
+		'serverName': 'n00dles'
+	},
+	{
+		'companyName': 'Nova Medical',
+		'stockSymbol': 'NVMD',
+		'serverName': 'nova-med'
+	},
+	{
+		'companyName': 'NWO',
+		'stockSymbol': '',
+		'serverName': 'nwo'
+	},
+	{
+		'companyName': 'Omega Software',
+		'stockSymbol': 'OMGA',
+		'serverName': 'omega-net'
+	},
+	{
+		'companyName': 'Omnia Cybersystems',
+		'stockSymbol': 'OMN',
+		'serverName': 'omnia'
+	},
+	{
+		'companyName': 'OmniTek Incorporated',
+		'stockSymbol': 'OMTK',
+		'serverName': 'omnitek'
+	},
+	{
+		'companyName': 'Phantasy Club',
+		'stockSymbol': '',
+		'serverName': 'phantasy'
+	},
+	{
+		'companyName': 'Powerhouse Fitness',
+		'stockSymbol': '',
+		'serverName': 'powerhouse-fitness'
+	},
+	{
+		'companyName': 'Rho Contruction',
+		'stockSymbol': 'RHOC',
+		'serverName': 'rho-construction'
+	},
+	{
+		'companyName': 'Rothman University',
+		'stockSymbol': '',
+		'serverName': 'rothman-uni'
+	},
+	{
+		'companyName': 'The Runners',
+		'stockSymbol': '',
+		'serverName': 'run4theh111z'
+	},
+	{
+		'companyName': 'Sigma Cosmetics',
+		'stockSymbol': 'SGC',
+		'serverName': 'sigma-cosmetics'
+	},
+	{
+		'companyName': 'Silver Helix',
+		'stockSymbol': '',
+		'serverName': 'silver-helix'
+	},
+
+	{
+		'companyName': 'Summit University',
+		'stockSymbol': '',
+		'serverName': 'summit-uni'
+	},
+	{
+		'companyName': 'Snap Fitness',
+		'stockSymbol': '',
+		'serverName': 'snap-fitness'
+	},
+	{
+		'companyName': 'Solaris Space Systems',
+		'stockSymbol': 'SLRS',
+		'serverName': 'solaris'
+	},
+	{
+		'companyName': 'Storm Technologies',
+		'stockSymbol': 'STM',
+		'serverName': 'stormtech'
+	},
+	{
+		'companyName': 'SysCore Securities',
+		'stockSymbol': 'SYSC',
+		'serverName': 'syscore'
+	},
+	{
+		'companyName': 'Taiyang Digital',
+		'stockSymbol': '',
+		'serverName': 'taiyang-digital',
+	},
+	{
+		'companyName': 'The Hub',
+		'stockSymbol': '',
+		'serverName': 'the-hub',
+	},
+	{
+		'companyName': 'Titan Laboratories',
+		'stockSymbol': 'TITN',
+		'serverName': 'titan-labs'
+	},
+	{
+		'companyName': 'UnitaLife Group',
+		'stockSymbol': '',
+		'serverName': 'unitalife',
+	},
+	{
+		'companyName': 'Universal Energy',
+		'stockSymbol': 'UNV',
+		'serverName': 'univ-energy'
+	},
+	{
+		'companyName': 'VitaLife',
+		'stockSymbol': 'VITA',
+		'serverName': 'vitalife'
+	},
+	{
+		'companyName': 'Watchdog Security',
+		'stockSymbol': 'WDS',
+		'serverName': 'NoServer'
+	},
+	{
+		'companyName': 'w0r1d_d43m0n',
+		'stockSymbol': '',
+		'serverName': 'w0r1d_d43m0n'
+	},
+	{
+		'companyName': 'ZB Institute of Technology',
+		'stockSymbol': '',
+		'serverName': 'zb-institute',
+	},
+	{
+		'companyName': 'ZER0 Nightclub',
+		'stockSymbol': '',
+		'serverName': 'zer0'
+	},
+	{
+		'companyName': 'ZB Defense Industries',
+		'stockSymbol': '',
+		'serverName': 'zb-def'
+	},
+	{
+		'companyName': 'Zeus Medical',
+		'stockSymbol': '',
+		'serverName': 'zeus-med'
+	},
+]
